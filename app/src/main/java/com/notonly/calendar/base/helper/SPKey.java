a9db8049968d250fd6a2928891198aead1dcbf97
王震 <lavalike@yeah.net>
2016-11-22 08:58:20 +0800

    详情页面顶部Toolbar改为半透明

--- app/src/main/java/com/notonly/calendar/base/helper/SPKey.java
+++ app/src/main/java/com/notonly/calendar/base/helper/SPKey.java
@@ -6,5 +6,8 @@ package com.notonly.calendar.base.helper;
  */
 
 public class SPKey {
-    
+    //列表展示方式
+    public static String KEY_LIST_MODE = "list_mode";
+    public static String LIST_MODE_LIST = "list_mode_list";
+    public static String LIST_MODE_STAGGER = "list_mode_stagger";
 }
